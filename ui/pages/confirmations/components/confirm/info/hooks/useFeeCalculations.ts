6a8612c24b10a34d8c72c338201b9d7a58234a13
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2026-07-13 13:22:57 +0100

    Revert "fix(confirmations): include protection gas overhead for swaps"
    This reverts commit d76cde5fb837886552af79c92506f9138655a0f4.

--- ui/pages/confirmations/components/confirm/info/hooks/useFeeCalculations.ts
+++ ui/pages/confirmations/components/confirm/info/hooks/useFeeCalculations.ts
@@ -45,7 +45,7 @@ export function useFeeCalculations(transactionMeta: TransactionMeta) {
   const hasValidConversionRate =
     Number.isFinite(conversionRate) && Number(conversionRate) > 0;
 
-  const { gasLimit: optimizedGasLimit } =
+  const { gasLimit: optimizedGasLimit, quotedGasLimit } =
     useTransactionGasLimit(transactionMeta);
 
   const getFeesFromHex = useCallback(
@@ -108,7 +108,7 @@ export function useFeeCalculations(transactionMeta: TransactionMeta) {
   const gasFeeEstimate = useTransactionGasFeeEstimate(
     transactionMeta,
     supportsEIP1559,
-    optimizedGasLimit,
+    quotedGasLimit,
   );
 
   const { gasFeeEstimates } = useGasFeeEstimates(
